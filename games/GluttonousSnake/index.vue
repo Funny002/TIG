079d94805060ce1d86a9ce8f6d5a7f21d362922e
Funny <funnydsu@gmail.com>
2023-08-10 15:16:13 +0800

    backup

--- games/GluttonousSnake/index.vue
+++ games/GluttonousSnake/index.vue
@@ -6,37 +6,44 @@
 
 <script lang="ts">export default { name: 'Gluttonous Snake' };</script>
 <script setup lang="ts">
-import { computed, onMounted, reactive, ref } from 'vue';
-import { KeyBinding } from '@utils/BindingKey';
-import { CreateCollision } from 'tig-core';
-
-const root = ref<HTMLDivElement>();
-const canvas = ref<HTMLCanvasElement>();
-
-const data = reactive<{
-  core: CreateCollision | null;
-  keyBinding: KeyBinding | null;
-}>({
-  core: null,
-  keyBinding: null,
-});
-const boxSize = computed(() => {
-  const { offsetWidth, offsetHeight } = root.value || { offsetWidth: 0, offsetHeight: 0 };
-  return { width: offsetWidth, height: offsetHeight };
-});
-
-onMounted(() => {
-  if (root.value) initKeyBinding(root.value);
-  if (canvas.value) initCreate(canvas.value);
-});
-
-function initKeyBinding(dom: HTMLDivElement) {
-  data.keyBinding = new KeyBinding(dom);
-}
-
-function initCreate(canvas: HTMLCanvasElement) {
-  data.core = new CreateCollision(canvas, boxSize.value);
-}
+// import { computed, onMounted, reactive, ref } from 'vue';
+// import { createSnakeItem } from './utils/Snake.ts';
+// import { KeyBinding } from '@utils/BindingKey';
+// import { Create } from 'tig-core/src';
+// const root = ref<HTMLDivElement>();
+// const canvas = ref<HTMLCanvasElement>();
+// const data = reactive<{
+//   core: Create | null;
+//   keyBinding: KeyBinding | null;
+// }>({
+//   core: null,
+//   keyBinding: null,
+// });
+// const boxSize = computed(() => {
+//   const { offsetWidth, offsetHeight } = root.value || { offsetWidth: 0, offsetHeight: 0 };
+//   return { width: offsetWidth, height: offsetHeight };
+// });
+// onMounted(() => {
+//   if (root.value) initKeyBinding(root.value);
+//   if (canvas.value) initCreate(canvas.value);
+// });
+// function initKeyBinding(dom: HTMLDivElement) {
+//   data.keyBinding = new KeyBinding(dom);
+// }
+// function initCreate(canvas: HTMLCanvasElement) {
+//   // data.core = new Create(canvas, boxSize.value);
+//   // data.core.run();
+//   // for (let i = 0; i < 5; i++) {
+//   //   const x = createSnakeItem(10, 50 - i * 10);
+//   //   data.core.insert(x);
+//   // }
+// }
 </script>
 
-<style lang="scss" src="./style.scss"/>
+<style lang="scss">
+.var-GluttonousSnake {
+  width: 100%;
+  height: 100%;
+  outline: none;
+}
+</style>
---
+++ games/GluttonousSnake/utils/Graphics.ts
@@ -0,0 +1,13 @@
+import { Shape } from 'tig-core/src';
+
+// TODO: 蛇
+export class Snake {
+  public speed: number = 2;
+  public children: Shape[] = [];
+  public direction: 0 | 1 | 2 | 3 = 0;
+
+  // TODO: 移动
+  move() {
+    // children[0]
+  }
+}
