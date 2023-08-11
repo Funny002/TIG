a6fcaad9ffc8771c815994e119083128fde23d97
Funny <funnydsu@gmail.com>
2023-08-11 17:04:28 +0800

    update

--- games/GluttonousSnake/index.vue
+++ games/GluttonousSnake/index.vue
@@ -6,38 +6,104 @@
 
 <script lang="ts">export default { name: 'Gluttonous Snake' };</script>
 <script setup lang="ts">
-// import { computed, onMounted, reactive, ref } from 'vue';
-// import { createSnakeItem } from './utils/Snake.ts';
-// import { KeyBinding } from '@utils/BindingKey';
-// import { Create } from 'tig-core/src';
-// const root = ref<HTMLDivElement>();
-// const canvas = ref<HTMLCanvasElement>();
-// const data = reactive<{
-//   core: Create | null;
-//   keyBinding: KeyBinding | null;
-// }>({
-//   core: null,
-//   keyBinding: null,
-// });
-// const boxSize = computed(() => {
-//   const { offsetWidth, offsetHeight } = root.value || { offsetWidth: 0, offsetHeight: 0 };
-//   return { width: offsetWidth, height: offsetHeight };
-// });
-// onMounted(() => {
-//   if (root.value) initKeyBinding(root.value);
-//   if (canvas.value) initCreate(canvas.value);
-// });
-// function initKeyBinding(dom: HTMLDivElement) {
-//   data.keyBinding = new KeyBinding(dom);
-// }
-// function initCreate(canvas: HTMLCanvasElement) {
-//   // data.core = new Create(canvas, boxSize.value);
-//   // data.core.run();
-//   // for (let i = 0; i < 5; i++) {
-//   //   const x = createSnakeItem(10, 50 - i * 10);
-//   //   data.core.insert(x);
-//   // }
-// }
+import { computed, onMounted, reactive, ref } from 'vue';
+import { KeyBinding } from '@utils/BindingKey';
+import { Snake } from './utils/Graphics.ts';
+import { Create } from 'tig-core/src';
+
+const root = ref<HTMLDivElement>();
+const canvas = ref<HTMLCanvasElement>();
+const data = reactive<{
+  stop: boolean;
+  core: Create | null;
+  snake: Snake | null;
+  keyBinding: KeyBinding | null;
+  timeout: NodeJS.Timeout | null;
+}>({
+  core: null,
+  stop: true,
+  snake: null,
+  timeout: null,
+  keyBinding: null,
+});
+
+const boxSize = computed(() => {
+  const { offsetWidth, offsetHeight } = root.value || { offsetWidth: 0, offsetHeight: 0 };
+  return { width: offsetWidth, height: offsetHeight };
+});
+
+onMounted(() => {
+  if (root.value) initKeyBinding(root.value);
+  if (canvas.value) initCreate(canvas.value);
+});
+
+function initKeyBinding(dom: HTMLDivElement) {
+  data.keyBinding = new KeyBinding(dom);
+}
+
+function initCreate(canvas: HTMLCanvasElement) {
+  data.core = new Create(canvas, boxSize.value);
+  data.core.on('FPS', console.log);
+  data.stop = false;
+  data.core.run();
+  initSnake();
+  Run();
+  // for (let i = 0; i < 5; i++) {
+  //   const x = createSnakeItem(10, 50 - i * 10);
+  //   data.core.insert(x);
+  // }
+}
+
+// =====================================================
+function initSnake() {
+  const snake = data.snake = new Snake(<Create>data.core, boxSize.value);
+  data.snake.addChild(0, 2);
+  data.snake.addChild(0, 1);
+  data.snake.addChild(0, 0);
+
+  root.value?.addEventListener('keydown', (e) => {
+    switch (e.key) {
+      case 'ArrowUp': {
+        snake.direction = 0;
+        break;
+      }
+      case 'ArrowDown': {
+        snake.direction = 1;
+        break;
+      }
+      case 'ArrowLeft': {
+        snake.direction = 2;
+        break;
+      }
+      case 'ArrowRight': {
+        snake.direction = 3;
+        break;
+      }
+    }
+  });
+}
+
+function StopToggleStatus() {
+  data.stop = !data.stop;
+  if (!data.stop) {
+    Run();
+  } else {
+    Stop();
+  }
+}
+
+// run
+function Run() {
+  if (data.stop) return false;
+  data.snake?.move();
+  data.timeout = setTimeout(Run, 100);
+}
+
+// stop
+function Stop() {
+  clearTimeout(<NodeJS.Timeout>data.timeout);
+}
+
 </script>
 
 <style lang="scss">
--- games/GluttonousSnake/utils/Graphics.ts
+++ games/GluttonousSnake/utils/Graphics.ts
@@ -1,13 +1,57 @@
-import { Shape } from 'tig-core/src';
+import { Create, Shape, Rect } from 'tig-core/src';
+
+const boxSize = 10;
 
 // TODO: 蛇
 export class Snake {
   public speed: number = 2;
-  public children: Shape[] = [];
+  private readonly core: Create;
+  private children: Shape[] = [];
+  private readonly width: number;
+  private readonly height: number;
+  // TODO: 方向 0: 上, 1: 下, 2: 左, 3: 右
   public direction: 0 | 1 | 2 | 3 = 0;
+
+  constructor(core: Create, size: { width: number, height: number }) {
+    this.core = core;
+    this.width = Math.ceil(size.width / boxSize);
+    this.height = Math.ceil(size.height / boxSize);
+  }
+
+  addChild(x: number, y: number) {
+    const rect = new Rect(y * boxSize, x * boxSize, boxSize, boxSize);
+    this.children.unshift(rect);
+    this.core.insert(rect);
+  }
 
   // TODO: 移动
   move() {
-    // children[0]
+    const head = this.children[0];
+    let [x, y] = [head.left / boxSize, head.top / boxSize];
+    switch (this.direction) {
+      case 0: { // 上
+        y -= 1;
+        y = y < 0 ? this.height - 1 : y;
+        break;
+      }
+      case 1: { // 下
+        y += 1;
+        y = y >= this.height ? 0 : y;
+        break;
+      }
+      case 2: { // 左
+        x -= 1;
+        x = x < 0 ? this.width - 1 : x;
+        break;
+      }
+      case 3: { // 右
+        x += 1;
+        x = x >= this.width ? 0 : x;
+        break;
+      }
+    }
+    const foot = this.children.pop();
+    foot?.remove();
+    this.addChild(x, y);
   }
 }
